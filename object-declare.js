f2063d7a7bd44e23ad6733ebe7f4eea40214a0e3
Asaduzzaman Sunam <asaduzzaman.sunam@gmail.com>
2022-08-06 12:40:07 +0600

    object declared with 6 properties including array,object and method that return property

--- object-declare.js
+++ object-declare.js
@@ -1,1 +1,22 @@
-//Declare an object which has 5 properties (number,string,boolean,array,method that will return a property);
+//Declare an object which has atleast 5 properties (number,string,boolean,array, object,method that will return a property);
+
+const university = {
+  name: "Ahsanullah University of Science and Tech",
+  student: 7000,
+  department: ["Architechture", "BBA", "CSE", "Civil", "EEE", "Mechanical"],
+  ugcApproved: true,
+  location: {
+    street: "142-143 Love Road",
+    area: "Tejgaon",
+    district: "Dhaka",
+    country: "Bangladesh",
+  },
+
+  method: function () {
+    const info = `${this.name} has ${this.student} students.`;
+    return info;
+  },
+};
+
+const statement = university.method();
+console.log(statement);
